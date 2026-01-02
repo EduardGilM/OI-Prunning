e60fa7ed9dbcb4b8bca31ed3667dc3723701ae21
EduardGil <eduardgilmagraner@gmail.com>
2026-01-02 23:06:32 +0100

    Diablo

--- check_model.py
+++ check_model.py
@@ -1,3 +1,4 @@
 from transformers import ViTForImageClassification
 model = ViTForImageClassification.from_pretrained("google/vit-base-patch16-224", num_labels=100, ignore_mismatched_sizes=True)
 print(model.vit.encoder.layer[0])
+ 
